f25048d94ecbb25208cf7f54667ce2a5c9b48d94
RoyerYampasi <royeryl03@gmail.com>
2023-10-06 16:17:46 -0300

    subiendo integrador

---
+++ rick_and_morty/About.jsx
@@ -0,0 +1,10 @@
+export default function About() {
+    return (
+       <div>
+        <h1>Hola Mundo </h1>
+        <h2>About</h2>
+        <p>Royer Rolando Yampasi Laura</p>
+       </div>
+    );
+ }
+ 
