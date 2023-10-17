45f6701f7425ac729c286c93711235738e273045
RoyerYampasi <royeryl03@gmail.com>
2023-10-17 07:00:00 -0300

    Server Backend

---
+++ rick_and_morty/server/src/index.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const {data} = require("./utils/data");
+const fs =require('fs');
+
+function encontrarObjetoPorId(array, id) {
+    for (let i = 0; i < array.length; i++) {
+
+        if (array[i].id === Number(id)) {
+
+            return array[i];
+        }
+    }
+    return null;
+}
+
+http.createServer((req,res)=>{
+    res.setHeader('Access-Control-Allow-Origin', '*');//nos permite que cualquiera mande una peticion
+
+    let {url}=req;
+    let id;
+    if (/\/rickandmorty\/character\/\d+/.test(url)) {
+        url=url.split("/");
+        id=url.pop();
+        url=url.join("/")
+    }
+    
+
+    switch (url) {
+        case "/rickandmorty/character":
+ 
+            res.writeHead(200,{"Content-Type":"application/json"})
+            
+            res.end(JSON.stringify(encontrarObjetoPorId(data, id)))
+            break;
+
+        default:
+            res.writeHead(400);
+            res.end()
+            break;
+    }
+
+
+}).listen(3001,"localhost");//server localhost:3001rsrs
