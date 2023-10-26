b80fcf2bdf6eeec48192ecc830448f1674011ff0
RoyerYampasi <royeryl03@gmail.com>
2023-10-25 22:08:24 -0300

    trabajando con express

--- rick_and_morty/server/src/controllers/getCharById.js
+++ rick_and_morty/server/src/controllers/getCharById.js
@@ -1,19 +1,28 @@
 const axios =require('axios')
-const getCharById=(res,id)=>{
+
+const express =require('express');
+const routerCharacter=express.Router()
+const getCharById=(req,res)=>{
+    const id=req.params.id
+
+
+    const URL= `https://rym2.up.railway.app/api/character/${id}`;
+    
     axios
         .get(`https://rickandmortyapi.com/api/character/${id}`)
         .then((response)=> {
             let data={
-                id:response.data.id,
-                name:response.data.name,
-                gender:response.data.gender,
-                species:response.data.species,
-                origin:response.data.origin,
-                image:response.data.image,
-                status:response.data.status}
-                
-                res.writeHead(200,{"Content-Type":"application/json"})
-                res.end(JSON.stringify(data))
+                    id:response.data.id,
+                    name:response.data.name,
+                    gender:response.data.gender,
+                    species:response.data.species,
+                    origin:response.data.origin,
+                    image:response.data.image,
+                    status:response.data.status}
+            
+                // res.writeHead(200,{"Content-Type":"application/json"})
+                // res.end(JSON.stringify(data))
+                res.status(200).json(data);
         })
         .catch((error)=>{
             res.writeHead(500,{"Content-Type":"text/plain"})
@@ -21,7 +30,7 @@ const getCharById=(res,id)=>{
         })
 }
 
-module.exports = {
-    getCharById
-};
+routerCharacter.get('/:id',(req,res)=>{ getCharById(req,res)})
+
+module.exports = routerCharacter
    
--- rick_and_morty/server/src/index.js
+++ rick_and_morty/server/src/index.js
@@ -1,44 +1,74 @@
-const http = require('http');
-const {data} = require("./utils/data");
-const fs =require('fs');
-const {getCharById}=require('./controllers/getCharById')
-function encontrarObjetoPorId(array, id) {
-    for (let i = 0; i < array.length; i++) {
+// const http = require('http');
+// const {data} = require("./utils/data");
+// const fs =require('fs');
+// const {getCharById}=require('./controllers/getCharById')
 
-        if (array[i].id === Number(id)) {
+// function encontrarObjetoPorId(array, id) {
+//     for (let i = 0; i < array.length; i++) {
 
-            return array[i];
-        }
-    }
-    return null;
-}
+//         if (array[i].id === Number(id)) {
 
-http.createServer((req,res)=>{
-    res.setHeader('Access-Control-Allow-Origin', '*');//nos permite que cualquiera mande una peticion
+//             return array[i];
+//         }
+//     }
+//     return null;
+// }
 
-    let {url}=req;
-    let id;
-    if (/\/rickandmorty\/character\/\d+/.test(url)) {
-        url=url.split("/");
-        id=url.pop();
-        url=url.join("/")
-    }
+// http.createServer((req,res)=>{
+//     res.setHeader('Access-Control-Allow-Origin', '*');//nos permite que cualquiera mande una peticion
+
+//     let {url}=req;
+//     let id;
+//     if (/\/rickandmorty\/character\/\d+/.test(url)) {
+//         url=url.split("/");
+//         id=url.pop();
+//         url=url.join("/")
+//     }
     
 
-    switch (url) {
-        case "/rickandmorty/character":
-            getCharById(res,id);
+//     switch (url) {
+//         case "/rickandmorty/character":
+//             getCharById(res,id);
 
-            // res.writeHead(200,{"Content-Type":"application/json"})
+//             // res.writeHead(200,{"Content-Type":"application/json"})
             
-            // res.end(JSON.stringify(encontrarObjetoPorId(data, id)))
-            break;
+//             // res.end(JSON.stringify(encontrarObjetoPorId(data, id)))
+//             break;
 
-        default:
-            res.writeHead(400);
-            res.end()
-            break;
+//         default:
+//             res.writeHead(400);
+//             res.end()
+//             break;
+//     }
+
+
+// }).listen(3001,"localhost");//server localhost:3001rsrs
+
+const express = require('express');
+const { getCharById } = require('./controllers/getCharById');
+const routerCharacter =require('./controllers/getCharById');
+const PORT = 3001;
+const server = express();
+
+server.use(express.json())
+server.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173'); // Reemplaza con tu dominio permitido
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  
+    // Habilita cookies y credenciales (si es necesario)
+    res.header('Access-Control-Allow-Credentials', 'true');
+  
+    if (req.method === 'OPTIONS') {
+      // Respuesta preflights CORS
+      res.status(204).send();
+    } else {
+      next();
     }
+  })
+server.use('/rickandmorty/character',routerCharacter)
 
 
-}).listen(3001,"localhost");//server localhost:3001rsrs
+server.listen(PORT, () => {
+   console.log('Server raised in port: ' + PORT);
+});
